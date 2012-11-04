35eb4108eae237be82a76e1b172ce53cd14f1272
Wentao Han <wentao.han@gmail.com>
2012-11-04 16:56:28 +0800

    fix a bug on timestamp.
    use datetime instead of time.

--- model/conference.rb
+++ model/conference.rb
@@ -3,10 +3,10 @@ class Conference < Sequel::Model
     primary_key :id
     varchar :acronym, :unique => true, :empty => false
     varchar :title,   :unique => true, :empty => false
-    varchar :url,                      :empty => false 
-    time    :abstract            
-    time    :deadline,                 :empty => false
-    time    :notification,             :empty => false
+    varchar :url,                      :empty => false
+    datetime :abstract
+    datetime :deadline,                 :empty => false
+    datetime :notification,             :empty => false
     varchar :address
     text    :cfp
   end
